d311877ed1ab35b5309f1abcc195d7756eaa4f29
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2024-10-10 17:06:47 +0300

    python

---
+++ PYTHON PROGRAMMING/control flow structures1.py
@@ -0,0 +1,31 @@
+# CONTROL FLOW STRUCTURES
+# Theses determin the order in which the pogram is executed basing on the loops and conditions.
+
+#CONDITIONAL STATEMENT
+#Thee are programs that base on a particulkar condition, eg. if, elif
+
+
+#Example.
+# 1. Create a program that asks a user for the food type bought from the market. 
+# The program should print yuo bought chicken if the user enters chicken,
+# print you brought liver if the user enters liver, elsi print the part of fish.
+
+
+
+food_type = input("Enter the type of food bought: ").lower()
+if food_type == 'chicken':
+    print(f"You bought chicken from the market")
+elif food_type == 'liver':
+    print(f"You bought fish from the market")
+elif food_type == 'fish':
+     print(f"You bought fish from the market")
+else:
+    print(f" choose from chicken liver and fish") # This is working with the conditional statements.
+
+
+
+# For another approach.
+if food_type != 'chicken' or food_type!= 'liver' or food_type!= 'fish':
+    print()
+
+    # MORE WORK
---
+++ PYTHON PROGRAMMING/loops.py
@@ -0,0 +1,2 @@
+# THE LOOPS.
+# These 
